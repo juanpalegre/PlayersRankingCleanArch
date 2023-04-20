487b5f91681b83037ae8fc0d39b50396ffe1b6ab
Juan Alegre <jpalegree@gmail.com>
2023-04-19 22:48:06 -0300

    Add mysql dependency to pom Create DatabaseConnector class

--- src/main/java/org/example/domain/DatabaseConnector.java
+++ src/main/java/org/example/domain/DatabaseConnector.java
@@ -1,4 +1,28 @@
 package org.example.domain;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    static Connection obtenerConexion(){
+        Connection connection = null;
+
+        try {
+            final String DRIVER = "com.mysql.jdbc.Driver";
+            final String URL = "jdbc:mysql://localhost:3306/playersranking";
+            final String USER = "root";
+            final String PASSWORD = "123456789";
+
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
